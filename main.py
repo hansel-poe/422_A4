0f5ffffc9f73a519853d4182567d6f42cd507b38
Hansel Poe <hanselivander68@yahoo.com>
2025-10-12 15:40:45 -0700

    Implemented parsing of samples

--- main.py
+++ main.py
@@ -4,13 +4,38 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Reads file from path and returns the content
+def get_file_contents(path):
+    file = open(path, 'r')
+    content = file.read()
+    file.close()
+    return content
+
+# parse the first n lines in str and returns dict count of rejected, pos, and neg samples
+def parse_n(str, n):
+    count_dict = {
+        "rej": 0,
+        "pos": 0,
+        "neg":0
+    }
+    samples = str.splitlines()
+    for i in range(n):
+        if samples[i] == '-1':
+            count_dict['rej'] += 1
+        elif samples[i] == '1':
+            count_dict['pos'] += 1
+        elif samples[i] == '2':
+            count_dict['neg'] += 1
+
+    return count_dict
+
+
+def main():
+    content = get_file_contents('./rs_1.csv')
+    print(parse_n(content,21424))
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
